b0e7be86822decb7e00febf9e248ff96811a80cf
Jason Xu <Xinwen31415@gmail.com>
2020-04-10 23:04:58 -0500

    Final form

---
+++ README.md
@@ -0,0 +1,10 @@
+# COVID-19 Model
+Use
+```
+	python sir-modeling.py
+```
+to generate beta and gamma values optimally fitted to data from real life in console, and US.png in the directory of the script.
+
+Original code by Lewuathe (https://github.com/Lewuathe/COVID19-SIR)
+
+Data provided by Johns Hopkins Whiting School of Engineering (<a href = "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series">here</a>)
--- sir-modeling.py
+++ sir-modeling.py
@@ -9,6 +9,7 @@ from scipy.optimize import minimize
 S_0 = 13500
 I_0 = 2
 R_0 = 0
+correction_factor = 13502/32720000
 
 class Learner(object):
 	def __init__(self, country, loss):
@@ -21,7 +22,15 @@ class Learner(object):
 		"""
 		df = pd.read_csv('data/time_series_19-covid-Confirmed.csv')
 		country_df = df[df['Country_Region'] == country]
-		return country_df.iloc[0].loc['1/22/20':]
+		# out = country_df.iloc[0].loc['1/22/20':'3/13/20']
+
+		# pre-quarantine: '1/22/20':'3/13/20'
+		# post-quarantine: '3/13/20':
+		
+		confirmed_sums = np.sum([reg.loc['1/22/20':].values for reg in country_df.iloc], axis = 0)
+		new_data = pd.DataFrame(confirmed_sums, country_df.iloc[0].loc['1/22/20':].index.tolist())
+		
+		return new_data
 
 	def load_recovered(self, country):
 		"""
@@ -44,7 +53,7 @@ class Learner(object):
 			Predict how the number of people in each compartment can be changed through time toward the future.
 			The model is formulated with the given beta and gamma.
 		"""
-		predict_range = 150
+		predict_range = len(data.index)
 		new_index = self.extend_index(data.index, predict_range)
 		size = len(new_index)
 		def SIR(t, y):
@@ -52,7 +61,7 @@ class Learner(object):
 			I = y[1]
 			R = y[2]
 			return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
-		extended_actual = np.concatenate((data.values, [None] * (size - len(data.values))))
+		extended_actual = np.concatenate((data.values.flatten(), [None] * (size - len(data.values))))
 		return new_index, extended_actual, solve_ivp(SIR, [0, size], [S_0,I_0,R_0], t_eval=np.arange(0, size, 1))
 
 	def train(self):
@@ -71,9 +80,12 @@ class Learner(object):
 		beta, gamma = optimal.x
 		print(f'Beta: {beta}, Gamma: {gamma}, R0: {beta/gamma}')
 		new_index, extended_actual, prediction = self.predict(beta, gamma, confirmed_data)
+		print(f'Predicted I: {prediction.y[1][-1]}, Actual I: {extended_actual[-1] * correction_factor}')
+		print(f'Predicted R: {prediction.y[2][-1]}, Actual R: {recovered_data[-1] * correction_factor}')
 		df = pd.DataFrame({
-			'Actual': extended_actual,
-			'S': prediction.y[0],
+			'Actual': extended_actual * correction_factor,
+			'Recovered': recovered_data.values * correction_factor,
+			# 'S': prediction.y[0],
 			'I': prediction.y[1],
 			'R': prediction.y[2]
 		}, index=new_index)
@@ -83,33 +95,32 @@ class Learner(object):
 		fig.savefig(f"{self.country}.png")
 
 # def loss(point, data):
-    # """
-    # RMSE between actual confirmed cases and the estimated infectious people with given beta and gamma.
-    # """
-    # size = len(data)
-    # beta, gamma = point
-    # def SIR(t, y):
-    #     S = y[0]
-    #     I = y[1]
-    #     R = y[2]
-    #     return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
-    # solution = solve_ivp(SIR, [0, size], [S_0,I_0,R_0], t_eval=np.arange(0, size, 1), vectorized=True)
-    # return np.sqrt(np.mean((solution.y[1] - data)**2))
+# 	"""
+# 		RMSE between actual confirmed cases and the estimated infectious people with given beta and gamma.
+# 	"""
+# 	size = len(data)
+# 	beta, gamma = point
+# 	def SIR(t, y):
+# 		S = y[0]
+# 		I = y[1]
+# 		R = y[2]
+# 		return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
+# 	solution = solve_ivp(SIR, [0, size], [S_0,I_0,R_0], t_eval=np.arange(0, size, 1), vectorized=True)
+# 	return np.sqrt(np.mean((solution.y[1] - data)**2))
 
-def loss(point, data, recovered):
-    size = len(data)
-    beta, gamma = point
-    def SIR(t, y):
-        S = y[0]
-        I = y[1]
-        R = y[2]
-        return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
-    solution = solve_ivp(SIR, [0, size], [S_0,I_0,R_0], t_eval=np.arange(0, size, 1), vectorized=True)
-    l1 = np.sqrt(np.mean((solution.y[1] - data)**2))
-    l2 = np.sqrt(np.mean((solution.y[2] - recovered)**2))
-    # Put more emphasis on recovered people
-    alpha = 0.1
-    return alpha * l1 + (1 - alpha) * l2
+def loss(point, confirmed, recovered):
+	size = len(confirmed)
+	beta, gamma = point
+	def SIR(t, y):
+		S = y[0]
+		I = y[1]
+		R = y[2]
+		return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
+	solution = solve_ivp(SIR, [0, size], [S_0,I_0,R_0], t_eval=np.arange(0, size, 1), vectorized=True)
+	sol_inf = np.sqrt(np.mean((solution.y[1] - (confirmed.values.flatten() * correction_factor))**2))
+	sol_rec = np.sqrt(np.mean((solution.y[2] - (recovered.values * correction_factor))**2))
+	# Put more emphasis on recovered people
+	return sol_inf * 0.1 + sol_rec * 0.9
 
 my_learner = Learner('US', loss)
 my_learner.train()
